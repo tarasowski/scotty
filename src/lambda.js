22edd5cd0589bbcd2e679b1ae9f07abceb4b1d2d
Dimitri Tarasowski <33748291+tarasowski@users.noreply.github.com>
2018-10-19 15:01:47 +0200

    Update lambda.js

--- src/lambda.js
+++ src/lambda.js
@@ -1,12 +1,16 @@
 const AWS = require('aws-sdk')
 const lambda = new AWS.Lambda({ region: 'eu-west-1' })
 
+const BUCKET_NAME = process.env.BUCKET_NAME
+
 module.exports.updateLambdaCode = (functionName, key) => {
+    if (!functionName) throw new Error('please provide a valid fuction name')
+    if (!key) throw new Error('please provide a valid key for the function')
     console.log('\x1b[32m%s\x1b[0m', `updating: ${key}`)
     const params = {
         FunctionName: functionName,
         Publish: true,
-        S3Bucket: 'cli-update-function-test',
+        S3Bucket: BUCKET_NAME,
         S3Key: key
     }
     return lambda.updateFunctionCode(params).promise()
