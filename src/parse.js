55858e52895d8474891f2d8d6c7c8cc64cd26c24
tarasowski <dimitri+github@tarasowski.de>
2018-10-20 08:11:37 +0200

    Fixed path navigation

--- src/parse.js
+++ src/parse.js
@@ -3,6 +3,6 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports.parseYaml = () => {
-    const templatePath = path.join(__dirname, '..', '..', '/template.yaml')
+    const templatePath = path.join(__dirname, '..', '..', '..', '/template.yaml')
     return yaml.safeLoad(fs.readFileSync(templatePath, 'utf-8'))
 }
